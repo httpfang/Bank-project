6d22252a09385560ad2d28a4b3cc35372ede7ce1
Harsh verma <114400511+httpfang@users.noreply.github.com>
2024-07-21 14:07:26 +0000

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
+
+
+
+// when we have so many of these components we dont want to import all in our app.jsx as it wont look cool so by using this trick we can import all components in our app.js file and then import all in our app.js file again and then import all in our app.js file again and then import all in our app.js file again and then import all components 
